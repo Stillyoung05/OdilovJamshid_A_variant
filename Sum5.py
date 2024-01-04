d59edb6e616270e3a0e5d1dec79bfe69587eb811
Stillyoung05 <97973124+Stillyoung05@users.noreply.github.com>
2024-01-04 19:46:38 +0500

    Sum5

---
+++ Sum5.py
@@ -0,0 +1,47 @@
+import time
+import asyncio
+
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print(f"{func.__name__} runtime: {execution_time} secs")
+        return result
+
+    return wrapper
+
+
+@timer
+def prime_numbers(n):
+    primes = []
+    for num in range(2, n + 1):
+        is_prime = True
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            primes.append(num)
+    print(f"Twin primes: {primes}")
+
+
+@timer
+async def async_prime_numbers(n):
+    primes = []
+    for num in range(2, n + 1):
+        is_prime = True
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            primes.append(num)
+        await asyncio.sleep(0.1)
+    print(f"ASync twin primes: {primes}")
+
+
+prime_numbers(1000)
+asyncio.run(async_prime_numbers(1000))
