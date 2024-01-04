589f2ac24be72dbbd765fae0c640937e796975db
Stillyoung05 <97973124+Stillyoung05@users.noreply.github.com>
2024-01-04 20:09:17 +0500

    update_sum3

--- Sum3.py
+++ Sum3.py
@@ -9,8 +9,32 @@ db_config = {
 }
 connection = psycopg2.connect(**db_config)
 cursor = connection.cursor()
-table_name = 'exam_table'
-query = """select from * """
+empl_cre = """CREATE TABLE employees (
+    employee_id SERIAL PRIMARY KEY,
+    employee_name VARCHAR(255) NOT NULL,
+    department_id INT NOT NULL,
+    salary DECIMAL(10, 2) NOT NULL
+);"""
+cursor.execute(empl_cre)
+dp_cre = """CREATE TABLE departments (
+    department_id SERIAL PRIMARY KEY,
+    department_name VARCHAR(255) NOT NULL
+);"""
+cursor.execute(dp_cre)
+projects_cre="""CREATE TABLE projects (
+    project_id SERIAL PRIMARY KEY,
+    project_name VARCHAR(255) NOT NULL,
+    employee_id INT NOT NULL,
+    FOREIGN KEY (employee_id) REFERENCES employees(employee_id)
+);"""
+cursor.execute(projects_cre)
+query = """SELECT departments.department_name, MAX(e.salary) AS max_salary
+FROM employees e
+JOIN departments d ON employees.department_id = departments.department_id
+GROUP BY departments.department_name
+ORDER BY max_salary DESC
+LIMIT 5;
+"""
 
 cursor.execute(query)
 rows = cursor.fetchall()
